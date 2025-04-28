228a558d8e4387790c5fefb674865a92a743f49a
Anjan Bellamkonda <bellamkonda.anjan.usa@gmail.com>
2025-04-28 14:08:17 -0400

    scrapped frontend

--- frontend/src/utils/auth.js
+++
@@ -1,6 +0,0 @@
-// src/utils/auth.js
-export function getCookie(name) {
-    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    return match ? match[2] : null;
-  }
-  
