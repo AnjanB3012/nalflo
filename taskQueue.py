32ab2d4e45759e62a16b26de57b7d7ba40ad858a
Anjan Bellamkonda <147215718+AnjanB3012@users.noreply.github.com>
2025-06-08 10:10:36 -0400

    mac update

--- taskQueue.py
+++ taskQueue.py
@@ -1,7 +1,7 @@
 import time
 import os
-from google import genai
-from google.genai import types
+import google.generativeai as genai
+from google.generativeai import types
 import json
 from dotenv import load_dotenv
 import requests
